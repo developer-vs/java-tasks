f1a02e62f866889f846b0a81e688b5ed6a7baad6
Vladimir Semenovich <37423497+developer-vs@users.noreply.github.com>
2020-01-15 13:27:56 -0800

    code refactoring

--- hw_8_1/DateUtilsTest.java
+++ hw_8_1/DateUtilsTest.java
@@ -3,7 +3,6 @@ package hw_8_1;
 public class DateUtilsTest {
 
     public static void main(String[] args) {
-        ;
         for(int i = 0; i < DateUtils.getMonths().length; i++) {
             System.out.println("month: " + DateUtils.getMonths()[i].getMonth() +
                 ", days: " + DateUtils.getMonths()[i].getDaysInMonth() +
